9cf1154222232fa9d4cb45430fca26e947823d60
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-01 16:13:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,6 +75,7 @@ MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 
 # Others
+PORT = environ.get("PORT", "8080")
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")

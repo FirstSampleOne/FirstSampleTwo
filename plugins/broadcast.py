fcbd402f543fa2da2acb8613fe98f7f19253c852
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-24 21:55:50 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,218 +1,124 @@
-import datetime, time, os, asyncio,logging 
-from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from pyrogram.types import Message, InlineKeyboardButton
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
+import time
+import asyncio
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.users_chats_db import db
 from info import ADMINS
+from utils import temp
 
-        
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast(bot, message):
+lock = asyncio.Lock()
+
+@Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
+async def broadcast_cancel(bot, query):
+    _, ident = query.data.split("#")
+    if ident == 'users':
+        await query.message.edit("Trying to cancel users broadcasting...")
+        temp.USERS_CANCEL = True
+    elif ident == 'groups':
+        await query.message.edit("Trying to cancel groups broadcasting...")
+        temp.GROUPS_CANCEL = True
+
+@Client.on_message(filters.command(["broadcast", "pinbroadcast"]) & filters.user(ADMINS) & filters.reply)
+async def users_broadcast(bot, message):
+    if lock.locked():
+        return await message.reply('Currently broadcasting is in progress. Please wait until it is completed.')
+    
+    pin = message.command[0] == 'pinbroadcast'
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text('Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ Yᴏᴜʀ Mᴇssᴀɢᴇs...')
+    b_sts = await message.reply_text(text='Broadcasting your message to users...')
     start_time = time.time()
     total_users = await db.total_users_count()
-    done = 0
-    blocked = 0
-    deleted = 0
-    failed =0
-    success = 0
-    async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
-        done += 1
-        if not done % 20:
-            await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ Iɴ Pʀᴏɢʀᴇss:\n\nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nBʟᴏᴄᴋᴇᴅ: {blocked}\nDᴇʟᴇᴛᴇᴅ: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    await bot.send_message(message.chat.id, f"Bʀᴏᴀᴅᴄᴀsᴛ Coᴍᴩʟᴇᴛᴇᴅ:\nTɪᴍᴇ Tᴀᴋᴇᴅ{time_taken} Sᴇᴄ\n\nTᴏᴛᴀʟ Uꜱᴇʀꜱ: {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSucᴄᴇꜱꜱ: {success}\nBʟᴏᴄᴋᴇᴅ: {blocked}\nDᴇʟᴇᴛᴇᴅ: {deleted}")
+    done, failed, success = 0, 0, 0
 
-
-@Client.on_message(filters.command("clear_junk") & filters.user(ADMINS))
-async def remove_junkuser__db(bot, message):
-    users = await db.get_all_users()
-    b_msg = message 
-    sts = await message.reply_text('IN PROGRESS.......')   
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    blocked = 0
-    deleted = 0
-    failed = 0
-    done = 0
-    async for user in users:
-        pti, sh = await clear_junk(int(user['id']), b_msg)
-        if pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
-        done += 1
-        if not done % 20:
-            await sts.edit(f"In Progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    await bot.send_message(message.chat.id, f"Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nBlocked: {blocked}\nDeleted: {deleted}")
-
-
-@Client.on_message(filters.command("group_broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_group(bot, message):
-    groups = await db.get_all_chats()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(text='Broadcasting your messages To Groups...')
-    start_time = time.time()
-    total_groups = await db.total_chat_count()
-    done = 0
-    failed = ""
-    success = 0
-    deleted = 0
-    async for group in groups:
-        pti, sh, ex = await broadcast_messages_group(int(group['id']), b_msg)
-        if pti == True:
-            if sh == "Succes":
+    async with lock:
+        async for user in users:
+            time_taken = get_readable_time(time.time() - start_time)
+            if temp.USERS_CANCEL:
+                temp.USERS_CANCEL = False
+                await b_sts.edit(f"Users broadcast cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
+                return
+            
+            sts = await broadcast_messages(int(user['id']), b_msg, pin)
+            if sts == 'Success':
                 success += 1
-        elif pti == False:
-            if sh == "deleted":
-                deleted+=1 
-                failed += ex 
-                try:
-                    await bot.leave_chat(int(group['id']))
-                except Exception as e:
-                    print(f"{e} > {group['id']}")  
-        done += 1
-        if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    try:
-        await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}\nDeleted: {deleted}\n\nFiled Reson:- {failed}")
-    except MessageTooLong:
-        with open('reason.txt', 'w+') as outfile:
-            outfile.write(failed)
-        await message.reply_document('reason.txt', caption=f"Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}\nDeleted: {deleted}")
-        os.remove("reason.txt")
+            else:
+                failed += 1
+            done += 1
+            
+            if done % 20 == 0:
+                btn = [[InlineKeyboardButton('CANCEL', callback_data='broadcast_cancel#users')]]
+                await b_sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
+        
+        await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
 
-      
-@Client.on_message(filters.command(["junk_group", "clear_junk_group"]) & filters.user(ADMINS))
-async def junk_clear_group(bot, message):
-    groups = await db.get_all_chats()
-    b_msg = message
-    sts = await message.reply_text(text='..............')
+@Client.on_message(filters.command(["grp_broadcast", "pin_grp_broadcast"]) & filters.user(ADMINS) & filters.reply)
+async def groups_broadcast(bot, message):
+    if lock.locked():
+        return await message.reply('Currently broadcasting is in progress. Please wait until it is completed.')
+    
+    pin = message.command[0] == 'pin_grp_broadcast'
+    chats = await db.get_all_chats()
+    b_msg = message.reply_to_message
+    b_sts = await message.reply_text(text='Broadcasting your message to groups...')
     start_time = time.time()
-    total_groups = await db.total_chat_count()
-    done = 0
-    failed = ""
-    deleted = 0
-    async for group in groups:
-        pti, sh, ex = await junk_group(int(group['id']), b_msg)        
-        if pti == False:
-            if sh == "deleted":
-                deleted+=1 
-                failed += ex 
-                try:
-                    await bot.leave_chat(int(group['id']))
-                except Exception as e:
-                    print(f"{e} > {group['id']}")  
-        done += 1
-        if not done % 20:
-            await sts.edit(f"in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    try:
-        await bot.send_message(message.chat.id, f"Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nDeleted: {deleted}\n\nFiled Reson:- {failed}")    
-    except MessageTooLong:
-        with open('junk.txt', 'w+') as outfile:
-            outfile.write(failed)
-        await message.reply_document('junk.txt', caption=f"Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nDeleted: {deleted}")
-        os.remove("junk.txt")
+    total_chats = await db.total_chat_count()
+    done, failed, success = 0, 0, 0
 
-async def broadcast_messages_group(chat_id, message):
+    async with lock:
+        async for chat in chats:
+            time_taken = get_readable_time(time.time() - start_time)
+            if temp.GROUPS_CANCEL:
+                temp.GROUPS_CANCEL = False
+                await b_sts.edit(f"Groups broadcast cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+                return
+            
+            sts = await groups_broadcast_messages(int(chat['id']), b_msg, pin)
+            if sts == 'Success':
+                success += 1
+            else:
+                failed += 1
+            done += 1
+            
+            if done % 20 == 0:
+                btn = [[InlineKeyboardButton('CANCEL', callback_data='broadcast_cancel#groups')]]
+                await b_sts.edit(f"Groups broadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))
+        
+        await b_sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+
+async def broadcast_messages(user_id, message, pin):
     try:
-        await message.copy(chat_id=chat_id)
-        return True, "Succes", 'mm'
+        m = await message.copy(chat_id=user_id)
+        if pin:
+            await m.pin(both_sides=True)
+        return "Success"
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return await broadcast_messages_group(chat_id, message)
-    except Exception as e:
-        await db.delete_chat(int(chat_id))       
-        logging.info(f"{chat_id} - PeerIdInvalid")
-        return False, "deleted", f'{e}\n\n'
-    
-async def junk_group(chat_id, message):
+        return await broadcast_messages(user_id, message, pin)
+    except Exception:
+        return "Error"
+
+async def groups_broadcast_messages(chat_id, message, pin):
     try:
-        kk = await message.copy(chat_id=chat_id)
-        await kk.delete(True)
-        return True, "Succes", 'mm'
+        k = await message.copy(chat_id=chat_id)
+        if pin:
+            try:
+                await k.pin()
+            except:
+                pass
+        return "Success"
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return await junk_group(chat_id, message)
-    except Exception as e:
-        await db.delete_chat(int(chat_id))       
-        logging.info(f"{chat_id} - PeerIdInvalid")
-        return False, "deleted", f'{e}\n\n'
-    
+        return await groups_broadcast_messages(chat_id, message, pin)
+    except Exception:
+        return "Error"
 
-async def clear_junk(user_id, message):
-    try:
-        key = await message.copy(chat_id=user_id)
-        await key.delete(True)
-        return True, "Success"
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await clear_junk(user_id, message)
-    except InputUserDeactivated:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id}-Removed from Database, since deleted account.")
-        return False, "Deleted"
-    except UserIsBlocked:
-        logging.info(f"{user_id} -Blocked the bot.")
-        return False, "Blocked"
-    except PeerIdInvalid:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id} - PeerIdInvalid")
-        return False, "Error"
-    except Exception as e:
-        return False, "Error"
-
-
-
-async def broadcast_messages(user_id, message):
-    try:
-        await message.copy(chat_id=user_id)
-        return True, "Success"
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await broadcast_messages(user_id, message)
-    except InputUserDeactivated:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id}-Removed from Database, since deleted account.")
-        return False, "Deleted"
-    except UserIsBlocked:
-        logging.info(f"{user_id} -Blocked the bot.")
-        return False, "Blocked"
-    except PeerIdInvalid:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id} - PeerIdInvalid")
-        return False, "Error"
-    except Exception as e:
-        return False, "Error"
-
-
-
-
-
-
-
-
-
+def get_readable_time(seconds):
+    periods = [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]
+    result = ''
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            result += f'{int(period_value)}{period_name}'
+    return result

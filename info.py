4b26da0036060e86e908a2e4042a05464dcb46f9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-07-28 19:23:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,6 +16,7 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
